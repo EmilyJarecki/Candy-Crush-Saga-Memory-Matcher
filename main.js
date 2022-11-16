2e15b48c3d29484c098fe8ce576029e5de99b62b
EmilyJarecki <jareckiemily@yahoo.com>
2022-11-15 19:56:04 -0600

    small cardArray created

---
+++ main.js
@@ -0,0 +1,16 @@
+cardArray = [
+    {
+        name : "Pizza", 
+        img : "pizza.img"
+    },
+    {
+        name : "Hot Dog", 
+        img : "hotdog.img"
+    },
+    {
+        name : "Ice Cream",
+        img : "icecream.img"
+    }
+]
+
+
