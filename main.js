eef746a9de8f2c2dec13b3408fa60ca6d2b0fe28
EmilyJarecki <jareckiemily@yahoo.com>
2022-11-15 21:47:50 -0600

    a for loop that creates a grid

--- main.js
+++ main.js
@@ -15,5 +15,16 @@ cardArray = [
 const grid = document.querySelector(".grid")
 console.log(grid)
 
+function createBoard(){
+    for (let i = 0; i < cardArray.length; i++){
+        console.log(cardArray[i])
+        card = document.createElement("img")//creating an image of card so we are creating an image element
+        // card.setAttribute("src", cardArray[i])
+        // card.setAttribute("value", i)
+        grid.appendChild(card)
+        console.log(grid)
+    } 
+}
 
+createBoard()
 
